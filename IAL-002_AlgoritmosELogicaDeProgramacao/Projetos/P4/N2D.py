a59a9213ac617316c52d292271c4dbb42cfbb9bd
Ikedaaaa <matheus-ikeda@hotmail.com>
2023-06-24 23:35:08 -0300

    Código do Projeto 4
    Código do Projeto 4: Estoque Operacional
    - Pequenos ajustes (perfumaria) e "log" do que está sendo executado no momento;
    - Gravação das informações nos seus devidos arquivos

--- IAL-002_AlgoritmosELogicaDeProgramacao/Projetos/P4/N2D.py
+++ IAL-002_AlgoritmosELogicaDeProgramacao/Projetos/P4/N2D.py
@@ -15,13 +15,13 @@ def getProductIdxById(Id):
 
 def getDivergency(errorCode, lineNumber, productId):
     if errorCode == 135:
-        return f"Linha {lineNumber} – Venda cancelada"
+        return f"Linha {lineNumber} - Venda cancelada"
     elif errorCode == 190:
-        return f"Linha {lineNumber} – Venda não finalizada"
+        return f"Linha {lineNumber} - Venda não finalizada"
     elif errorCode == 999:
-        return f"Linha {lineNumber} – Erro desconhecido. Acionar equipe de TI."
+        return f"Linha {lineNumber} - Erro desconhecido. Acionar equipe de TI"
     else:
-        return f"Linha {lineNumber} – Código de Produto não encontrado {productId}"
+        return f"Linha {lineNumber} - Código de Produto não encontrado {productId}"
 
 produtos = []
 vendas = []
@@ -30,6 +30,7 @@ divergencias = []
 totcanais = [["1 - Representantes", 0], ["2 - Website", 0], ["3 - App móvel Android", 0], ["4 - App móvel iPhone", 0]]
 
 #******************* ENTRADA DE DADOS ********************
+print("Lendo arquivo PRODUTOS.TXT")
 with open('PRODUTOS.TXT', 'r') as arquivoProdutos:
     infoProduto = arquivoProdutos.readline().rstrip()
     while infoProduto != "":
@@ -38,6 +39,7 @@ with open('PRODUTOS.TXT', 'r') as arquivoProdutos:
         transferencias.append([infoProdutoAsList[0], 0, infoProdutoAsList[1], 0, 0])
         infoProduto = arquivoProdutos.readline().rstrip()
 
+print("Lendo arquivo VENDAS.TXT")
 with open('VENDAS.TXT', 'r') as arquivoVendas:
     infoVenda = arquivoVendas.readline().rstrip()
     while infoVenda != "":
@@ -45,6 +47,8 @@ with open('VENDAS.TXT', 'r') as arquivoVendas:
         infoVenda = arquivoVendas.readline().rstrip()
 
 #************* PROCESSAMENTO DOS DADOS DE ENTRADA **************
+
+print("\nProcessando os dados de entrada\n")
 for idx, venda in enumerate(vendas):
     productIdx = getProductIdxById(venda[0])
 
@@ -66,4 +70,30 @@ while i < len(transferencias):
     transferencias[i][4] = 10 if 1 < transferencias[i][3] < 10 else transferencias[i][3] #Transf. de Arm p/ CO
     i += 1
 
+#*********** GRAVAÇÃO DOS DADOS NOS DEVIDOS ARQUIVOS ************
 
+print("Gravando informações no arquivo TRANSFERE.TXT\n")
+
+arqTransfLineFmt = "{:<7}{:>6}{:>7}{:>10}{:>11}{:>9}{:>12}\n"
+with open("TRANSFERE.TXT", "w") as arquivoTransferencias:
+    arquivoTransferencias.write("Necessidade de Transferência Armazém para CO\n\n")
+    arquivoTransferencias.write(arqTransfLineFmt.format("Produto", "QtCO", "QtMin", "QtVendas", "Estq.após", "Necess.", "Transf. de"))
+    arquivoTransferencias.write(arqTransfLineFmt.format("", "", "", "", "Vendas", "", "Arm p/ CO"))
+
+    for idx, transferencia in enumerate(transferencias):
+        arquivoTransferencias.write(arqTransfLineFmt.format(transferencia[0], produtos[idx][1], produtos[idx][2], transferencia[1], transferencia[2], transferencia[3], transferencia[4]))
+
+print("Gravando informações no arquivo DIVERGENCIAS.TXT\n")
+with open("DIVERGENCIAS.TXT", "w") as arquivoDivergencias:
+    for divergencia in divergencias:
+        arquivoDivergencias.write(divergencia+"\n")
+
+print("Gravando informações no arquivo TOTCANAIS.TXT\n")
+with open("TOTCANAIS.TXT", "w") as arquivoTotcanais:
+    arquivoTotcanais.write("Quantidades de Vendas por canal\n\n")
+    arquivoTotcanais.write("{:<23}{:>8}\n".format("Canal", "QtVendas"))
+
+    for totcanal in totcanais:
+        arquivoTotcanais.write("{:<23}{:>8}\n".format(totcanal[0], totcanal[1]))
+
+print("******************** Fim do Programa ********************")
