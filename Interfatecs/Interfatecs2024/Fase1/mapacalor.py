55fcce8a6326dffe0a19adcbcc5961bfa88fc194
Ikedaaaa <matheus-ikeda@hotmail.com>
2024-05-26 16:23:34 -0300

    Interfatecs 2024 - Fase 1: Mapacalor
    Talvez essa solução apresentaria a resposta correta

--- Interfatecs/Interfatecs2024/Fase1/mapacalor.py
+++ Interfatecs/Interfatecs2024/Fase1/mapacalor.py
@@ -1,42 +1,33 @@
-#WRONG ANSWER
+#MAYBE RIGHT ANSWER
 n = int(input())
-lista = []
-for inputs in range(n*6):
-    lista.append(list(map(int,input().split())))
-
-regioes = [0, 0, 0, 0, 0]
-nomes = ['superior', 'esquerda', 'centro', 'direita', 'inferior']
+pts = [0, 0, 0, 0, 0]
+reg = ["superior", "esquerda", "centro", "direita", "inferior"]
 aux = 0
+matriz = []
+for i in range(n*6):
+	matriz.append([int(x) for x in input().split()])
 
-for i in range(n):
-    if sum(lista[aux]) == 3:
-        regioes[0] += 1
-    else:
-        soma = 0
-        for a in range(1, 5):
-            soma += lista[a][0]
-        if soma == 4:
-            regioes[1] += 1
-        else:
-            somaEsq, somaCen, somaDir = 0, 0, 0
-            for a in range(1, 5):
-                somaEsq += lista[a+aux][0]
-                somaCen += lista[a+aux][1]
-                somaDir += lista[a+aux][2]
-            
-            if(somaEsq == 4): 
-                regioes[1] += 1
-            
-            elif(somaCen == 4):
-                regioes[2] += 1
-            
-            elif(somaDir == 4):
-                regioes[3] += 1
+for tela in range(n):
+	if(sum(matriz[aux]) == 3):
+		pts[0] += 1
 
-            elif sum(lista[aux+5]) == 3:
-                regioes[4] += 1
+	sumE, sumC, sumD = 0, 0, 0
+	for i in range(1,5):
+		sumE += matriz[i+aux][0]
+		sumC += matriz[i+aux][1]
+		sumD += matriz[i+aux][2]
+	
+	if(sumE == 4): 
+		pts[1] += 1
+	if(sumC == 4): 
+		pts[2] += 1
+	if(sumD == 4): 
+		pts[3] += 1
+	
+	if(sum(matriz[aux+5]) == 3):
+		pts[4] += 1
+	aux += 6
 
-    aux += 6
-
-maximo = max(regioes)
-print(nomes[regioes.index(maximo)])
+#print(pts)
+maximo = max(pts)
+print(reg[pts.index(maximo)])
