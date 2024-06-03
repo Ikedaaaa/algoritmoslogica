134118a9683d4a6638f76cc75c6497099e86b51c
Ikedaaaa <matheus-ikeda@hotmail.com>
2024-06-03 01:58:55 -0300

    Interfatecs 2024 - Fase 1: Rotaciona
    Ajuste para os resultados utilizarem, como orientado no desafio, pi = 3.1415 na conversão de graus para radianos, o que torna os resultados de acordo com o solicitado

--- Interfatecs/Interfatecs2024/Fase1/rotaciona.py
+++ Interfatecs/Interfatecs2024/Fase1/rotaciona.py
@@ -1,20 +1,15 @@
 import math
 coords, degrees = tuple(map(int, input().split()))
 
-coordinates_list = []
+coordinates = []
 for coord in range(coords):
-    coordinates_list.append(list(map(int, input().split())))
+    coordinates.append(list(map(int, input().split())))
 
-cos = math.cos(math.radians(degrees))
-sin = math.sin(math.radians(degrees))
+radians = degrees * (3.1415/180)
+cos = math.cos(radians)
+sin = math.sin(radians)
 
-for point in coordinates_list:
+for point in coordinates:
     x = (cos*point[0]) + (-sin*point[1])
     y = (sin*point[0]) + (cos*point[1])
     print(f"{x:.2f} {y:.2f}")
-
-# math.cos() returns the cosine of a given number in radians.
-# the input number is in degrees.
-# to calculate it correctly, it's needed to convert from degrees to radians
-# if not, math.cos() would return the value of the cosine of 40 radians, not 40 degrees
-# for that, math.radians(40) is used to convert 40 degrees to radians
